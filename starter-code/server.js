07665b37c1ac4ca642d46b9da5b7c8b63eb32fa2
ZacharyJohnson <zaj6827@gmail.com>
2017-07-28 10:17:33 -0700

    finished remaining TODOs and REVIEWs in server.js

--- starter-code/server.js
+++ starter-code/server.js
@@ -8,7 +8,7 @@ const requestProxy = require('express-request-proxy'); // REVIEW: We've added a 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = 'postgres:postgres:pass@localhost:5432:kilovolt'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
@@ -19,7 +19,7 @@ app.use(express.static('./public'));
 
 
 // COMMENT: What is this function doing? Why do we need it? Where does it receive a request from?
-// (put your response in a comment here)
+// This function is calling the github api, and passing in the token as params. We're using this function to make sure that our GITHUB_TOKEN is hidden inside the environment, for our security.
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
@@ -30,7 +30,7 @@ function proxyGitHub(request, response) {
 
 
 // COMMENT: What is this route doing? Where does it receive a request from?
-// (put your response in a comment here)
+// The route is returning information to the user based on the user's request.
 app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
 app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
 app.get('/github/*', proxyGitHub);
@@ -107,7 +107,7 @@ app.post('/articles', function(request, response) {
 
 
 // COMMENT: What is this route doing? Where does it receive a request from?
-// (put your response in a comment here)
+// This route is for the user's input when making a new article. Using SQL, we're pushing the information they gave us into the authors table with the author information, and pushing the actual article content into the articles table. If we need both at once, we can always use a JOIN to get all the information at once.
 app.put('/articles/:id', (request, response) => {
   client.query(`
     UPDATE authors
